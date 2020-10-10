906d187c4dd9ab30c2dd1cb03300c3823ffaf74c
Dj Seymur <dj.seymur@outlook.com>
2020-10-10 23:13:34 +0400

    Updates

---
+++ precache-manifest.7e0087f7b990a6f9297d9fb1f04202ac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a88c544f34841b0dfc8c1e88579617",
+    "url": "/skycafe/index.html"
+  },
+  {
+    "revision": "8b84c445cf716ee3ae12",
+    "url": "/skycafe/static/css/main.e688c02e.chunk.css"
+  },
+  {
+    "revision": "55cc4ad29327365bb2ab",
+    "url": "/skycafe/static/js/2.d53fe6fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/skycafe/static/js/2.d53fe6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b84c445cf716ee3ae12",
+    "url": "/skycafe/static/js/main.779d25f7.chunk.js"
+  },
+  {
+    "revision": "f1e6f09024676bf14ee9",
+    "url": "/skycafe/static/js/runtime-main.a0157901.js"
+  },
+  {
+    "revision": "50409fdb454241a33370a07c8bb14a5b",
+    "url": "/skycafe/static/media/coffee-foam.50409fdb.png"
+  },
+  {
+    "revision": "648bdfc5adbbb30d6f45b4a902891435",
+    "url": "/skycafe/static/media/findus-circle.648bdfc5.svg"
+  },
+  {
+    "revision": "197958c2e3c115af8eb3997336d17ff7",
+    "url": "/skycafe/static/media/left-pic.197958c2.jpg"
+  },
+  {
+    "revision": "a5d80e44793718583b3dfeedaea7d798",
+    "url": "/skycafe/static/media/pie-circle.a5d80e44.svg"
+  },
+  {
+    "revision": "61933649cc69f13c2354764ca6d99cbf",
+    "url": "/skycafe/static/media/right-pic.61933649.jpg"
+  }
+]);
